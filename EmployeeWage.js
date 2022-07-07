e27663c42428bde55e8bdac283870b70c43e24e2
julinbestina <julin.bestina@yahoo.com>
2022-07-07 23:53:45 +0530

    Refactored the code to calculate Employee Wage using Function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,19 +4,23 @@ const FULL_TIME_HOUR = 8;
 const EMP_FULLTIME = 1;
 const EMP_PARTTIME = 2;
 const WAGE_PER_HOUR = 20;
-let empHour = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3; 
 
-switch(empCheck) {
-    case EMP_FULLTIME: 
-        empHour = FULL_TIME_HOUR;
-        break;
-    case EMP_PARTTIME:
-        empHour = PART_TIME_HOUR;
-        break;
-    default:
-        empHour = 0;
+let empHour = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+
+function getWorkingHours(empCheck) {
+    switch(empCheck) {
+        case EMP_FULLTIME: 
+            return FULL_TIME_HOUR;
+            break;
+        case EMP_PARTTIME:
+            return PART_TIME_HOUR;
+            break;
+        default:
+            return 0;
+    }
 }
 
+empHour = getWorkingHours(empCheck);
 let empWage = empHour * WAGE_PER_HOUR;
 console.log("Employee wage :" + empWage);
