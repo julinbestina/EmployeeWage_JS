9b7d6d3ff351a0a408092b375aad9bc0471777d3
julinbestina <julin.bestina@yahoo.com>
2022-07-07 23:41:17 +0530

    Added code to calculate Employee Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,22 @@
-const ABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-console.log(empCheck);
-if(empCheck == ABSENT) {
-    console.log("Employee is Absent");
-}
-else {
-    console.log("Employee is Present");
+
+const PART_TIME_HOUR = 4;
+const FULL_TIME_HOUR = 8;
+const EMP_FULLTIME = 1;
+const EMP_PARTTIME = 2;
+const WAGE_PER_HOUR = 20;
+let empHour = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3; 
+
+switch(empCheck) {
+    case EMP_FULLTIME: 
+        empHour = FULL_TIME_HOUR;
+        break;
+    case EMP_PARTTIME:
+        empHour = PART_TIME_HOUR;
+        break;
+    default:
+        empHour = 0;
 }
 
+let empWage = empHour * WAGE_PER_HOUR;
+console.log("Employee wage :" + empWage);
